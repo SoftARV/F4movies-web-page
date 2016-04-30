a9454755ca06810b504a108bc5eaa0d55d4a7a1c
SoftARV <miguelaezak@gmail.com>
2016-04-30 17:18:37 -0430

    feat: shopping cart menu created

--- modules/modules.php
+++ modules/modules.php
@@ -8,6 +8,7 @@ function Head($Titulo){
 			<link rel="stylesheet" type="text/css" href="css/style-index.css">
 			<link rel="stylesheet" type="text/css" href="css/style-login.css">
 			<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+			<link rel="stylesheet" href="css/style-carrito.css">
 			';
 }	
 
@@ -44,7 +45,7 @@ function Menu(){
 	            </ul>
 	            <ul class="nav navbar-nav navbar-right">
 	            	<button type="button" class="btn btn-inverted navbar-btn launch-modal" data-modal-id="modal-login">Iniciar Sesion</button>
-					<li><a href=""><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></a></li>
+					<li><a class="launch-modal" data-modal-id="modal-car"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></a></li>
 	            </ul>
 	        </div>
 	        <!-- /.navbar-collapse -->
@@ -164,6 +165,45 @@ function Menu(){
         	</div>
        	</div>
     </div>
+
+    <!-- modal shopping cart -->
+	<div class="modal fade" id="modal-car" tabindex="-1" role="dialog" aria-labelledby="modal-cart-label" aria-hidden="true">
+       	<div class="modal-dialog">
+        	<div class="modal-content">
+
+        		<div class="modal-header">
+        			<button type="button" class="close" data-dismiss="modal">
+        				<span aria-hidden="true">&times;</span><span class="sr-only">Close</span>
+        			</button>
+        			<h3 class="modal-title" id="modal-login-label">Carrito de Compra</h3>
+        		</div>
+
+        		<div class="modal-body">
+	                <ul>
+	                	<li>
+	                		<div class="row">
+	                			<div class="col-md-2 poster-movie">
+	                				<img src="http://ia.media-imdb.com/images/M/MV5BMjQyMzgzNTYzNF5BMl5BanBnXkFtZTgwNjA5OTQ3ODE@._V1_UX182_CR0,0,182,268_AL_.jpg" width="60" alt="">
+	                			</div>
+	                			<div class="col-md-8 description-movie">
+	                				<h2>Titulo Pelicula</h2>
+	                				<p>Precio...</p>
+	                			</div>
+	                			<div class="col-md-2 remove-movie">
+	                				<a href=""><img src="images/delete_sign.png" alt=""></a>
+	                			</div>
+	                		</div>
+	                	</li>
+	                </ul>
+        		</div>
+
+        		<div class="modal-footer">
+        			<button type="button" class="btn btn-primary checkout">Finalizar Compra</button>
+        		</div>
+
+        	</div>
+       	</div>
+    </div>
 	
 		';
 	}
@@ -190,12 +230,53 @@ function Menu(){
 		            </ul>
 		            <ul class="nav navbar-nav navbar-right">
 		            	<li><a href="#">'.$_SESSION["usuario"].'</a></li>
-						<li><a href=""><img src="images/shopping_cart.png" title="carrito de compra" width="12"></a></li>
+						<li><a class="launch-modal" data-modal-id="modal-car"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></a></li>
 		            	<li><a href="modules/logout.php">Cerrar sesion</a></li>
 		            </ul>
 		        </div>
 		    </div>
-		</nav>';
+		</nav>
+		
+		<!-- modal shopping cart -->
+	<div class="modal fade" id="modal-car" tabindex="-1" role="dialog" aria-labelledby="modal-cart-label" aria-hidden="true">
+       	<div class="modal-dialog">
+        	<div class="modal-content">
+
+        		<div class="modal-header">
+        			<button type="button" class="close" data-dismiss="modal">
+        				<span aria-hidden="true">&times;</span><span class="sr-only">Close</span>
+        			</button>
+        			<h3 class="modal-title" id="modal-login-label">Carrito de Compra</h3>
+        		</div>
+
+        		<div class="modal-body">
+	                <ul>
+	                	<li>
+	                		<div class="row">
+	                			<div class="col-md-2 poster-movie">
+	                				<img src="http://ia.media-imdb.com/images/M/MV5BMjQyMzgzNTYzNF5BMl5BanBnXkFtZTgwNjA5OTQ3ODE@._V1_UX182_CR0,0,182,268_AL_.jpg" width="60" alt="">
+	                			</div>
+	                			<div class="col-md-8 description-movie">
+	                				<h2>Titulo Pelicula</h2>
+	                				<p>Precio...</p>
+	                			</div>
+	                			<div class="col-md-2 remove-movie">
+	                				<a href=""><img src="images/delete_sign.png" alt=""></a>
+	                			</div>
+	                		</div>
+	                	</li>
+	                </ul>
+        		</div>
+
+        		<div class="modal-footer">
+        			<button type="button" class="btn btn-primary checkout">Finalizar Compra</button>
+        		</div>
+
+        	</div>
+       	</div>
+    </div>
+
+		';
 	}
 	}
 
