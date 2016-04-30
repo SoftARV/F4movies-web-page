30a507c5072b1ec12274bb7f589869b02573e3a5
SoftARV <miguelaezak@gmail.com>
2016-04-30 13:00:35 -0430

    feat: added owl carousel to the home page

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 <html lang="en">
 <head>
 	<?php echo Head("F4movies"); ?>
+	<link rel="stylesheet" type="text/css" href="owlcarousel/assets/owl.carousel.css">
 </head>
 <body>
 	<?php 
@@ -15,6 +16,7 @@
 	<article>
 
 		<div class="container description">
+		
 			<div class="row">
 				<div class="col-md-8 search-bar">
 					<h1 class="title">Bienvenido a F4movies</h1>
@@ -31,47 +33,24 @@
 					<img src="images/F4movies-navlogo.png" alt="logo" class="logo">
 				</div>
 			</div>
-		</div>
 
-		<div id="carousel" class="carousel slide" data-ride="carousel">
-		  	<!-- Indicators -->
-		  	<ol class="carousel-indicators">
-		    	<li data-target="#carousel" data-slide-to="0" class="active"></li>
-		    	<li data-target="#carousel" data-slide-to="1"></li>
-		    	<li data-target="#carousel" data-slide-to="2"></li>
-		  	</ol>
+			<div class="row">
+				<div class="col-md-12">
+				
+					<div class="owl-carousel">
+						<div><img src="http://ia.media-imdb.com/images/M/MV5BMjQyMzgzNTYzNF5BMl5BanBnXkFtZTgwNjA5OTQ3ODE@._V1_UX182_CR0,0,182,268_AL_.jpg" alt=""></div>
+						<div><img src="http://ia.media-imdb.com/images/M/MV5BMjQyMzgzNTYzNF5BMl5BanBnXkFtZTgwNjA5OTQ3ODE@._V1_UX182_CR0,0,182,268_AL_.jpg" alt=""></div>
+						<div><img src="http://ia.media-imdb.com/images/M/MV5BMjQyMzgzNTYzNF5BMl5BanBnXkFtZTgwNjA5OTQ3ODE@._V1_UX182_CR0,0,182,268_AL_.jpg" alt=""></div>
+						<div><img src="http://ia.media-imdb.com/images/M/MV5BMjQyMzgzNTYzNF5BMl5BanBnXkFtZTgwNjA5OTQ3ODE@._V1_UX182_CR0,0,182,268_AL_.jpg" alt=""></div>
+						<div><img src="http://ia.media-imdb.com/images/M/MV5BMjQyMzgzNTYzNF5BMl5BanBnXkFtZTgwNjA5OTQ3ODE@._V1_UX182_CR0,0,182,268_AL_.jpg" alt=""></div>
+						<div><img src="http://ia.media-imdb.com/images/M/MV5BMjQyMzgzNTYzNF5BMl5BanBnXkFtZTgwNjA5OTQ3ODE@._V1_UX182_CR0,0,182,268_AL_.jpg" alt=""></div>
+						<div><img src="http://ia.media-imdb.com/images/M/MV5BMjQyMzgzNTYzNF5BMl5BanBnXkFtZTgwNjA5OTQ3ODE@._V1_UX182_CR0,0,182,268_AL_.jpg" alt=""></div>
+						<div><img src="http://ia.media-imdb.com/images/M/MV5BMjQyMzgzNTYzNF5BMl5BanBnXkFtZTgwNjA5OTQ3ODE@._V1_UX182_CR0,0,182,268_AL_.jpg" alt=""></div>
+					</div>
 
-		  	<!-- Wrapper for slides -->
-		  	<div class="carousel-inner" role="listbox">
-		    	<div class="item active">
-		      		<img src="images/Star-Wars-The-Force-Awakens-Official-Poster-header.jpg" class="img-responsive" alt="Pelicula 1">
-		      		<div class="carousel-caption">
-		        		<h3>Star Wars: The Force Awakens</h3>
-		      		</div>
-		    	</div>
-		    	<div class="item">
-		      		<img src="images/deadpool_banner.jpg" class="img-responsive" alt="Pelicula 2">
-		      		<div class="carousel-caption">
-		        		<h3>Deadpool</h3>
-		      		</div>
-		    	</div>
-		    	<div class="item">
-		      		<img src="images/alice_through_the_looking_glass_banner.jpg" class="img-responsive" alt="Pelicula 3">
-		      		<div class="carousel-caption">
-		        		<h3>Alice Through the Looking Glass</h3>
-		      		</div>
-		    	</div>
-		  	</div>
+				</div>
+			</div>
 
-		  	<!-- Controls -->
-		  	<a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
-		    	<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-		    	<span class="sr-only">Previous</span>
-		  	</a>
-		  	<a class="right carousel-control" href="#carousel" role="button" data-slide="next">
-		    	<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-		    	<span class="sr-only">Next</span>
-		  	</a>
 		</div>
 
 	</article>
@@ -81,5 +60,7 @@
 	</footer>
 
 	<?php echo imports(); ?>
+	<script src="owlcarousel/owl.carousel.min.js"></script>
+	<script src="js/carousel.js"></script>
 </body>
 </html>
