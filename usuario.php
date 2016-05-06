ed3f878af76e130a0e2a405a7ded872b00b6f929
fbricenho <francisco_1412@hotmail.com>
2016-05-06 00:43:30 -0400

    updates usuario validado con perfil, logout fixed

--- usuario.php
+++ usuario.php
@@ -1,6 +1,11 @@
 <?php 
-		include("modules/modules.php");
 		session_start();
+		if (!isset($_SESSION["usuario"]))	
+			{
+				die(header("location: index.php"));
+			}else{
+				include("modules/modules.php");
+			}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,18 +21,28 @@
 
 	<div class="container">
 		<div class="jumbotron">
-		  <h1>Bienvenido Usuario!</h1>
+		  <h1 style="color: white;">Bienvenido Usuario!</h1>
 		  <p>Datos del usuario</p>
 		</div>
-		<div class="row">			
+		<div class="row">	
+		<?php 
+			
+		 ?>		
 			<div class="col-lg-2">
 				<div>
 					<div class="list-group">
 						  <a class="list-group-item disabled">Usuario</a>
 						  <a href="#" class="list-group-item">Carrito</a>
 						  <a href="#" class="list-group-item">Compras realizadas</a>
-						  <a class="list-group-item disabled">Genero</a>
-						  <a href="#" class="list-group-item">Vestibulum at eros</a>
+						  <?php 
+						  	if ($_SESSION['perfil'] == 2) {
+						  		echo '<a class="list-group-item disabled">Panel administrativo</a>
+						  		<a href="#" class="list-group-item">Reporte de usuarios</a>
+						  		<a href="#" class="list-group-item">Agregar peliculas</a>
+						  		<a href="#" class="list-group-item">Modificar peliculas</a>';
+						  	}
+						   ?>
+						  
 					</div>
 				</div>
 			</div>
